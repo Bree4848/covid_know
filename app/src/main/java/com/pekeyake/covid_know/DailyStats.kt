1f963b82d751b651b9d7261c1356880a37a2e3e1
Mlamuli Molefe <mlamuli4848@gmail.com>
2021-09-17 20:06:53 +0200

    added adView

--- app/src/main/java/com/pekeyake/covid_know/DailyStats.kt
+++ app/src/main/java/com/pekeyake/covid_know/DailyStats.kt
@@ -2,11 +2,15 @@ package com.pekeyake.covid_know
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.common.base.FinalizableWeakReference
+import com.google.firebase.firestore.FirebaseFirestore
 import com.pekeyake.covid_know.adapter.CovidStatsAdapter
 import com.pekeyake.covid_know.core.model.CovidStats
+import com.google.android.gms.ads.MobileAds
 
 class DailyStats : AppCompatActivity()
 {
@@ -14,10 +18,17 @@ class DailyStats : AppCompatActivity()
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_daily_stats)
+
+        //MobileAds.initialize(this) {}
+
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         val shareButton = findViewById<Button>(R.id.share)
 
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val firebaseFireStore = FirebaseFirestore.getInstance()
+        firebaseFireStore.collection("covidknow")
+            .get()
 
         val dailystatstoday = ArrayList<CovidStats>()
 
--- app/src/main/java/com/pekeyake/covid_know/HomeActivity.kt
+++ app/src/main/java/com/pekeyake/covid_know/HomeActivity.kt
@@ -10,18 +10,33 @@ import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.initialization.InitializationStatus
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
 
 class HomeActivity : AppCompatActivity()
 {
     lateinit var toggle : ActionBarDrawerToggle
+
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        MobileAds.initialize(this) {}
+
+        val mAdView: AdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+
         val vaccineSites: Button = findViewById(R.id.vaccinesites)
         val dailySites: Button = findViewById(R.id.dailystats)
         val covidNews: Button = findViewById(R.id.covidnews)
+        val vaccineFacts: Button = findViewById(R.id.vaccine_facts)
+
         val drawerlayout: DrawerLayout = findViewById(R.id.drawerlayout)
         val navView : NavigationView = findViewById(R.id.navigationview)
 
@@ -31,6 +46,10 @@ class HomeActivity : AppCompatActivity()
         }
         dailySites.setOnClickListener {
             val intent = Intent(this,DailyStats::class.java)
+            startActivity(intent)
+        }
+        vaccineFacts.setOnClickListener {
+            val intent = Intent(this, VaccineFacts::class.java)
             startActivity(intent)
         }
         covidNews.setOnClickListener {
